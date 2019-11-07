e15a957701727ee6ff7c00637fac437c73cd0a57
A <kazero062@gmail.com>
2019-11-07 09:30:19 +0900

    hotfix WriteLine 2nd Hello

--- chapter007/Program.cs
+++ chapter007/Program.cs
@@ -12,6 +12,7 @@ namespace chapter007
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("hotfix : Hello World");
+            Console.WriteLine("hotfix : 2nd Hello World");
         }
     }
 }
